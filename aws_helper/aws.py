f9ebae1f317906140ed5f69bb28a3333ab81cbf1
Welby Obeng <wobeng@yblew.com>
2017-02-25 07:04:18 -0500

    allow access to session

--- aws_helper/aws.py
+++ aws_helper/aws.py
@@ -10,11 +10,11 @@ class Aws:
     def __init__(self, session=None, profile_name=None, region_name=None):
 
         if session:
-            self._session = session
+            self.session = session
         else:
-            self._session = boto3.session.Session(profile_name=profile_name, region_name=region_name)
+            self.session = boto3.session.Session(profile_name=profile_name, region_name=region_name)
 
-        self.aws_gateway = Gateway(self._session)
-        self.aws_lambda = Lambda(self._session)
-        self.aws_log = Log(self._session)
-        self.aws_s3 = S3(self._session)
+        self.aws_gateway = Gateway(self.session)
+        self.aws_lambda = Lambda(self.session)
+        self.aws_log = Log(self.session)
+        self.aws_s3 = S3(self.session)
