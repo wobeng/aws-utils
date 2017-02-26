5edab52b781c180d9a3929e0d2ae2dac7488c73a
Welby Obeng <wobeng@yblew.com>
2017-02-25 21:19:06 -0500

    testing new setup tools

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     keywords='aws helper',
     packages=find_packages(),
     version=_lu_meta['version'],
-    dependency_links=['git+https://github.com/wobeng/helper.git#egg=y_helper-1.0.0'],
+    dependency_links=['git+https://github.com/wobeng/helper.git@master#egg=y_helper-1.0.0'],
     install_requires = ['y_helper==1.0.0']
 )
