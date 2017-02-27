a786b8096f2ab6c31b5eab2ba1298e4f5551120a
yblew <wobeng@yblew.com>
2017-02-27 15:34:56 -0500

    added load config

--- aws_helper/aws.py
+++ aws_helper/aws.py
@@ -5,9 +5,21 @@ from aws_lambda import Lambda
 from aws_log import Log
 from aws_s3 import S3
 
+import os
+from helper import misc
 
 class Aws:
+
     def __init__(self, session=None, profile_name=None, region_name=None):
+
+        # load instance if available
+        try:
+            import instance
+            profile_name = instance.PROFILE
+            region_name = instance.REGION
+        except BaseException as e:
+            pass
+
 
         if session:
             self.session = session
@@ -18,3 +30,18 @@ class Aws:
         self.aws_lambda = Lambda(self.session)
         self.aws_log = Log(self.session)
         self.aws_s3 = S3(self.session)
+
+    def load_config(self):
+
+        bucket = os.environ.get("CONFIG_BUCKET", None)
+        key = os.environ.get("KEY", None)
+
+        try:
+            import instance
+            bucket = instance.CONFIG_BUCKET
+            key = instance.KEY
+        except BaseException as e:
+            pass
+
+        config = self.aws_s3.get_json_object(bucket,key)
+        misc.import_env_vars(config)
