71f733b899946d24908045ba24a1323b5cbcaad3
Welby Obeng <wobeng@yblew.com>
2017-04-05 19:02:31 -0400

    finished testing

--- aws_helper/aws_gateway.py
+++ aws_helper/aws_gateway.py
@@ -1,22 +1,17 @@
 import requests
-import traceback
 from requests_aws4auth import AWS4Auth
 
 
 class Gateway:
     def __init__(self, session):
-        cred = session.get_credentials()
+        creds = session.get_credentials()
         self.auth = AWS4Auth(
-            cred.access_key,
-            cred.secret_key,
+            creds.access_key,
+            creds.secret_key,
             session.region_name,
-            "execute-api", session_token=cred.token
+            "execute-api",
+            session_token=creds.token
         )
-        print(cred.access_key)
-        print(cred.secret_key)
-        print(cred.token)
-        print(session.region_name)
-        print(traceback.print_stack(limit=6))
     def invoke(self, method, endpoint, data=None, json=None, params=None):
         method = getattr(requests, method)
         response = method(endpoint, data=data, params=params, json=json, auth=self.auth)
