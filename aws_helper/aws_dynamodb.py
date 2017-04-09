9181ab0e28a1f69c81f16616166260133bcb2f82
Welby Obeng <wobeng@yblew.com>
2017-04-09 12:14:16 -0400

    add new methods

--- aws_helper/aws_dynamodb.py
+++ aws_helper/aws_dynamodb.py
@@ -1,4 +1,22 @@
+import os
+
 class Dynamodb:
     def __init__(self, session):
         self.client = session.client("dynamodb")
         self.resource = session.resource("dynamodb")
+
+    def put_item(self, table, item):
+        table = self.resource.Table(os.environ[table])
+        table.put_item(Item=item)
+
+    def get_item(self, table, key):
+        table = self.resource.Table(os.environ[table])
+        response = table.get_item(Key=key, ConsistentRead=True)
+        if "Item" in response and response["Item"]:
+            return response["Item"]
+
+    def query(self,table,attributes):
+        table = self.resource.Table(os.environ[table])
+        response = table.query(**attributes)
+        if ["Items"] in response and response["Items"]:
+            return response["Items"]
