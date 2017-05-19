f16428b904d4852082af069001d8aa9be1ab93c8
yblew <wobeng@yblew.com>
2017-05-19 09:42:03 -0400

    testing

--- scripts/lambda.sh
+++ scripts/lambda.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-
-aws --version && git --version && pip --version && python --version
+pip install virtualenv
+virtualenv  venv
+venv/bin/pip install -r requirements.txt
+wget -O ./lambda.py $SCRIPT_URL/scripts/lambda.py
+chmod u+x lambda.py && python lambda.py
