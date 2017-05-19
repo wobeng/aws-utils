36a1de63a641dc0e88be258bee082d7c9231794a
yblew <wobeng@yblew.com>
2017-05-19 09:37:41 -0400

    testing

--- scripts/lambda.sh
+++ scripts/lambda.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-mkdir lambda_function
-
-shopt -s extglob dotglob
-mv !lambda_function lambda_function
-shopt -u dotglob
-
-ls
-ls lambda_function
-
-venv/bin/pip3 install --no-cache-dir -r requirements.txt -t lambda_function
-ls
-wget -O ./lambda.py $SCRIPT_URL/scripts/lambda.py
-chmod u+x lambda.py && python3 lambda.py
+aws --version && git --version && pip --version && python --version
