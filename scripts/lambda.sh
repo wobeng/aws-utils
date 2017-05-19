a83701e15b60701744839fa7b489b05ea8237202
yblew <wobeng@yblew.com>
2017-05-19 10:32:43 -0400

    testing

--- scripts/lambda.sh
+++ scripts/lambda.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update && apt-get -y install zip
-pip install lambda-uploader && pip install -r requirements.txt -t .
-zip -r lambda_function.zip .
+pip install --upgrade pip && pip install lambda-uploader && pip install -r requirements.txt -t .
+zip -q -r lambda_function.zip .
 wget -O ./lambda.py $SCRIPT_URL/scripts/lambda.py
 chmod u+x lambda.py && python lambda.py
 
