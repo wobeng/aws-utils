340713a2a9403adce027b374c1d9376cd6e44137
yblew <wobeng@yblew.com>
2017-05-19 09:22:15 -0400

    testing

--- scripts/lambda.sh
+++ scripts/lambda.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-pip3 install --upgrade pip3
-pip3 install virtualenv
-virtualenv -p `which python3` venv
-venv/bin/pip3 install --no-cache-dir -r requirements.txt
+
+mkdir lambda_function
+
+shopt -s extglob dotglob
+mv !lambda_function lambda_function
+shopt -u dotglob
+
+ls
+ls lambda_function
+
+venv/bin/pip3 install --no-cache-dir -r requirements.txt -t lambda_function
+ls
 wget -O ./lambda.py $SCRIPT_URL/scripts/lambda.py
 chmod u+x lambda.py && python3 lambda.py
