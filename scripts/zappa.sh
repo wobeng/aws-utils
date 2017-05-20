3479f20222ef87a9c6026a0aa97162709c2992a3
Welby Obeng <wobeng@yblew.com>
2017-05-20 09:59:18 -0400

    py2 to py3

--- scripts/zappa.sh
+++ scripts/zappa.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip install virtualenv
-virtualenv -p `which python3` venv
+virtualenv -p `which python` venv
 venv/bin/pip install -r requirements.txt
 wget -O ./zappa.py $SCRIPT_URL/scripts/zappa.py
-chmod u+x zappa.py && python3 zappa.py
+chmod u+x zappa.py && python zappa.py
