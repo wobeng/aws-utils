608acf383afe512ca6daa353e426137d849be022
yblew <wobeng@yblew.com>
2017-06-02 13:34:18 -0400

    updating

--- scripts/lambda.py
+++ scripts/lambda.py
@@ -1,22 +1,44 @@
+#!/usr/bin/env python3.6
 import json
+import logging
 import subprocess
 
 import os
 
-CONFIG_FILE = "{}/_uploader/{}.json".format(os.environ["CODEBUILD_SRC_DIR"], os.environ["BRANCH"])
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-if os.path.isfile(CONFIG_FILE):
 
-    config = dict(BRANCH=os.environ["BRANCH"], KEY=os.environ["KEY"], CONFIG_BUCKET=os.environ["CONFIG_BUCKET"])
+def absolute(p):
+    return os.path.join(os.environ['CODEBUILD_SRC_DIR'], p)
 
-    command = "lambda-uploader --no-build " \
-              "-c={0}/_uploader/{1}.json " \
-              "-s={2} " \
-              "-k=lambda/lambda-uploader.zip " \
-              "--variables='{3}'".format(os.environ["CODEBUILD_SRC_DIR"], os.environ["BRANCH"],
-                                         os.environ["ZAPPA_BUCKET"], json.dumps(config))
-    print(command)
-    subprocess.call(command, shell=True)
 
+def log_code(c):
+    logger.debug('CODE ==> ' + str(c))
+
+
+logger.info('Install requirement')
+code = subprocess.call('pip3.6 -r {} -t {}'.format(absolute('requirements.txt'), absolute('')),
+                       shell=True)
+log_code(code)
+
+logger.info('Zipping...')
+code = subprocess.call('zip -q -r lambda_function.zip {}'.format(absolute('')), shell=True)
+log_code(code)
+
+config_file = absolute('_uploader/{}.json'.format(os.environ['BRANCH']))
+
+if os.path.isfile(config_file):
+
+    logger.info('Starting {}'.format(os.environ['BRANCH'].capitalize()))
+
+    config = dict(BRANCH=os.environ['BRANCH'], KEY=os.environ['KEY'], CONFIG_BUCKET=os.environ['CONFIG_BUCKET'])
+
+    command = 'lambda-uploader --no-build -c={} -s={} -k=lambda/lambda-uploader.zip --variables="{}'''.format(
+        config_file, os.environ['ZAPPA_BUCKET'], json.dumps(config))
+    logger.debug(command)
+    code = subprocess.call(command, shell=True)
 else:
-    subprocess.call("No {}.json found!".format(os.environ["BRANCH"]), shell=True)
+    code = subprocess.call('No {}.json found!'.format(os.environ['BRANCH']), shell=True)
+
+log_code(code)
