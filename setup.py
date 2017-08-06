762ab5cb084234aa23c9906c33da3696cef8e3d0
Welby Obeng <wobeng@yblew.com>
2017-08-06 16:24:39 -0400

    refactored

--- setup.py
+++ setup.py
@@ -1,27 +1,14 @@
-import sys
-from subprocess import call
-
-import os
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-
-
-class MyInstall(install):
-    def run(self):
-        install.run(self)
-        requirements_file = os.path.join(os.getcwd(), "requirements.txt")
-        if os.path.isfile(requirements_file):
-            cmd = [sys.executable.replace("python", "pip"), "install", "-r", requirements_file]
-            call(cmd)
-
+from distutils.core import setup
 
 setup(
     name='aws-utils',
-    description='aws utils',
-    author='Welby Obeng',
-    keywords='aws utils',
-    packages=find_packages(),
-    cmdclass={'install': MyInstall},
+    version='1.0.0',
+    packages=['tests', 'aws_utils'],
+    url='https://github.com/wobeng/aws-utils',
+    license='',
+    author='wobeng',
+    author_email='wobeng@yblew.com',
+    description='aws utility functions',
     install_requires=[
         'boto3',
         'requests_aws4auth',
