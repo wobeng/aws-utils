b2f23a9812a4e721d89aa932b122b595ec48a251
welbyobeng <wobeng@yblew.com>
2017-08-06 10:54:20 -0400

    refactored

--- setup.py
+++ setup.py
@@ -1,37 +1,9 @@
-import ast
 import sys
 from subprocess import call
 
 import os
-import re
 from setuptools import setup, find_packages
 from setuptools.command.install import install
-
-
-def package_meta():
-    """Read __init__.py for global package metadata.
-    Do this without importing the package.
-    """
-    _version_re = re.compile(r'__version__\s+=\s+(.*)')
-    _url_re = re.compile(r'__url__\s+=\s+(.*)')
-    _license_re = re.compile(r'__license__\s+=\s+(.*)')
-
-    with open('aws_utils/__init__.py', 'rb') as ffinit:
-        initcontent = ffinit.read()
-        version = str(ast.literal_eval(_version_re.search(
-            initcontent.decode('utf-8')).group(1)))
-        url = str(ast.literal_eval(_url_re.search(
-            initcontent.decode('utf-8')).group(1)))
-        licencia = str(ast.literal_eval(_license_re.search(
-            initcontent.decode('utf-8')).group(1)))
-    return {
-        'version': version,
-        'license': licencia,
-        'url': url,
-    }
-
-
-_lu_meta = package_meta()
 
 
 class MyInstall(install):
@@ -46,12 +18,9 @@ class MyInstall(install):
 setup(
     name='aws-utils',
     description='aws utils',
-    url=_lu_meta['url'],
     author='Welby Obeng',
-    license=_lu_meta['license'],
     keywords='aws utils',
     packages=find_packages(),
-    version=_lu_meta['version'],
     cmdclass={'install': MyInstall},
     install_requires=[
         'boto3',
