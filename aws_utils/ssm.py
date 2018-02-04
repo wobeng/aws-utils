04d601f8dfb1e15fa2cef41230b4b083b309a176
Welby Obeng <wobeng@yblew.com>
2018-02-04 09:31:58 -0500

    make sure name is string

--- aws_utils/ssm.py
+++ aws_utils/ssm.py
@@ -6,7 +6,8 @@ class Ssm:
         self.client = session.client('ssm')
 
     def parameter(self, name, default=None):
-        value = os.environ.get(str(name), default)
+        name = str(name)
+        value = os.environ.get(name, default)
         if not value:
             try:
                 value = self.client.get_parameter(Name=name, WithDecryption=True)['Parameter']['Value']
