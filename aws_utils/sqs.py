f2f50dce6287c7a5f3d682d7df01a96052baef10
Welby Obeng <wobeng@yblew.com>
2018-04-11 10:40:24 -0400

    added sqs and swf

--- aws_utils/sqs.py
+++ aws_utils/sqs.py
@@ -6,6 +6,13 @@ class Sqs:
         self.queue_url = self.client.get_queue_url(QueueName=queue_url)['QueueUrl']
         return self
 
+    def send_message(self, **kwargs):
+        response = self.client.send_message(
+            QueueUrl=self.queue_url,
+            **kwargs
+        )
+        return response
+
     def send_message_batch(self, entries):
         response = self.client.send_message_batch(
             QueueUrl=self.queue_url,
