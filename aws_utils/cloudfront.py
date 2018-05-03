fb248af0641347fe0fd4290f42d591534e5608a9
Welby Obeng <wobeng@yblew.com>
2018-05-03 14:52:39 -0400

    CryptographyDeprecationWarning: signer and verifier have been deprecated. Please use sign and verify instead.

--- aws_utils/cloudfront.py
+++ aws_utils/cloudfront.py
@@ -23,7 +23,7 @@ def rsa_signer(message, key):
         password=None,
         backend=default_backend()
     )
-    signer = private_key.signer(padding.PKCS1v15(), hashes.SHA1())
+    signer = private_key.sign(padding.PKCS1v15(), hashes.SHA1())
     signer.update(message)
     return signer.finalize()
 
