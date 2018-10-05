3de0cb6a66c6e94861e19275006c5f5be10b2e18
yblew <wobeng@yblew.com>
2018-10-05 18:31:19 -0400

    added receive messages

--- aws_utils/sqs.py
+++ aws_utils/sqs.py
@@ -9,6 +9,14 @@ class Sqs:
         self.queue_url = self.client.get_queue_url(QueueName=queue_name)['QueueUrl']
         return self
 
+    def receive_messages(self, limit=10, **kwargs):
+        response = self.client.receive_message(
+            QueueUrl=self.queue_url,
+            MaxNumberOfMessages=limit,
+            **kwargs
+        )
+        return response
+
     def send_message(self, message, **kwargs):
         response = self.client.send_message(
             QueueUrl=self.queue_url,
