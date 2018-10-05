f0a676c22a6dfdddd982bf06f35d1a4d6d3d6bd0
yblew <wobeng@yblew.com>
2018-10-05 18:40:36 -0400

    added receive messages

--- aws_utils/sqs.py
+++ aws_utils/sqs.py
@@ -9,6 +9,25 @@ class Sqs:
         self.queue_url = self.client.get_queue_url(QueueName=queue_name)['QueueUrl']
         return self
 
+    def _run_batch(self, entries, function_name):
+        n = 10
+        new_entries = [entries[i * n:(i + 1) * n] for i in range((len(entries) + n - 1) // n)]
+
+        for ne in new_entries:
+            ne_copy = ne.copy()
+            get_out = False
+            while not get_out:
+                function_name = getattr(self.client, function_name)
+                response = function_name(
+                    QueueUrl=self.queue_url,
+                    Entries=ne_copy
+                )
+                if response.get('Failed', None):
+                    failed_ids = [f['Id'] for f in response['Failed']]
+                    ne_copy = [e for e in ne if e['Id'] in failed_ids]
+                else:
+                    get_out = True
+
     def receive_messages(self, limit=10, **kwargs):
         response = self.client.receive_message(
             QueueUrl=self.queue_url,
@@ -26,20 +45,7 @@ class Sqs:
         return response
 
     def send_message_batch(self, entries):
+        return self._run_batch(entries, 'send_message_batch')
 
-        n = 10
-        new_entries = [entries[i * n:(i + 1) * n] for i in range((len(entries) + n - 1) // n)]
-
-        for ne in new_entries:
-            ne_copy = ne.copy()
-            get_out = False
-            while not get_out:
-                response = self.client.send_message_batch(
-                    QueueUrl=self.queue_url,
-                    Entries=ne_copy
-                )
-                if response.get('Failed', None):
-                    failed_ids = [f['Id'] for f in response['Failed']]
-                    ne_copy = [e for e in ne if e['Id'] in failed_ids]
-                else:
-                    get_out = True
+    def delete_message_batch(self, entries):
+        return self._run_batch(entries, 'delete_message_batch')
