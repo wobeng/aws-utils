e899870c1ce609fac85a564ae99edc6ffb807000
Welby Obeng <wobeng@yblew.com>
2018-10-11 15:27:00 -0400

    added sns

--- aws_utils/sns.py
+++ aws_utils/sns.py
@@ -6,6 +6,7 @@ from json import dumps
 class Sns:
     def __init__(self, session):
         self.client = session.client('sns')
+        self.account_id = session.client('sts').get_caller_identity()['Account']
         self.region_name = session.region_name
 
     def __call__(self, group_name=None, region_name=None):
@@ -16,8 +17,7 @@ class Sns:
         return self
 
     def publish(self, subject, message):
-        account_id = self.client.get_caller_identity()['Account']
-        topic_arn = 'arn:aws:sns:{}:{}:{}'.format(self.region_name, account_id, self.group_name)
+        topic_arn = 'arn:aws:sns:{}:{}:{}'.format(self.region_name, self.account_id, self.group_name)
         self.client.publish(TopicArn=topic_arn, Message=message, Subject=subject)
 
     def send_exception_email(self, domain, event):
