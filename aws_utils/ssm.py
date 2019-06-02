90e1213ddb3b6c1afdf5cbbb5f0b47f0f4123e49
yblew <wobeng@yblew.com>
2019-06-02 11:30:20 -0400

    refactored

--- aws_utils/ssm.py
+++ aws_utils/ssm.py
@@ -4,7 +4,6 @@ import os
 class Ssm:
     def __init__(self, session):
         self.client = session.client('ssm')
-        self.db = session.client('dynamodb')
 
     def parameter(self, name, default=None):
         name = str(name)
